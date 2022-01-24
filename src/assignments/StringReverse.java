3c35df7b1a9ddc963b7f1ac5f9befabf08fee56e
2dirtyy <2dirttyy@gmail.com>
2022-01-23 21:49:14 -0500

    Completed assignment
    Reverse a String

--- src/assignments/StringReverse.java
+++ src/assignments/StringReverse.java
@@ -1,5 +1,20 @@
 package assignments;
 
 public class StringReverse {
-
+	
+	public static String reverse(String str) {
+		String newString = "";
+		
+		for (int i = 0; i < str.length(); i++) {
+			newString += str.charAt(str.length() - 1 - i);
+		}
+		
+		return newString;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(reverse("hello"));
+		System.out.println(reverse("olleh"));
+		System.out.println(reverse("goodbye"));
+	}
 }
