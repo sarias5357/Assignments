f008d5a8618fb3beee290d81cab794900830ae21
2dirtyy <2dirttyy@gmail.com>
2022-01-23 23:54:50 -0500

    Create countVowels method

--- src/assignments/Vowels.java
+++ src/assignments/Vowels.java
@@ -1,5 +1,19 @@
 package assignments;
 
 public class Vowels {
-
+	public static int countVowels(String str) {
+		int sum = 0;
+		
+		for (int i = 0; i < str.length(); i++) {
+			char character = Character.toUpperCase(str.charAt(i));
+			
+			if (character == 'A' || character == 'E' || character == 'I' || character == 'O' || character == 'U') {
+				// Increment sum and print out vowel
+				sum++;
+				System.out.println(character);
+			}
+		}
+		
+		return sum;
+	}
 }
