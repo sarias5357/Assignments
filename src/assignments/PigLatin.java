d0fb5d07918b8e0b464aaffd169e8a0fd58023b6
2dirtyy <2dirttyy@gmail.com>
2022-01-23 22:08:59 -0500

    Create isConsonant method

--- src/assignments/PigLatin.java
+++ src/assignments/PigLatin.java
@@ -27,5 +27,16 @@ public class PigLatin {
 			}
 			return ans;
 		}
+		
+		public static boolean isConsonant(char x) {
+			char[] consonants = { 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k' ,'l', 'm', 'n', 'p', 'q'
+					,'r', 's', 't', 'v', 'w','x', 'y', 'z' };
+			
+			for (int i = 0; i < consonants.length; i++) 
+				if (x == consonants[i])
+					return true;
+
+			return false;
+		}
 }
 	
