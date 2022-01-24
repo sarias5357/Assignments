cbb3727546548b3928f655c26cb60b0e30870a6d
2dirtyy <2dirttyy@gmail.com>
2022-01-23 23:46:32 -0500

    Add isPalindrome method

--- src/assignments/Palindrome.java
+++ src/assignments/Palindrome.java
@@ -1,5 +1,13 @@
 package assignments;
 
 public class Palindrome {
-
+	public static boolean isPalindrome(String x) {
+		for (int i = 0; i <= x.length() / 2; i++) {
+			if (x.charAt(i) != x.charAt(x.length() - i - 1)) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
 }
