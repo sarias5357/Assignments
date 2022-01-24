d7d62745f2e6cddbb19bdb9dab17a8e02420e7c2
2dirtyy <2dirttyy@gmail.com>
2022-01-24 00:31:56 -0500

    Implement a linked list

---
+++ src/assignments/LinkedList.java
@@ -0,0 +1,13 @@
+package assignments;
+
+/**
+ * Singly-Linked List
+ * 
+ * Access    Search    Insertion    Deletion
+ * O(n) 	 O(n)      O(1)         O(1)
+ *
+ */
+
+public class LinkedList {
+	
+}
