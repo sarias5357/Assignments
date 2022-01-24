4b1f7a1be42ec341790039dc0d165883ac204016
2dirtyy <2dirttyy@gmail.com>
2022-01-24 00:24:34 -0500

    Update Count words assignment to support reading in text files

--- src/assignments/Vowels.java
+++ src/assignments/Vowels.java
@@ -1,5 +1,9 @@
 package assignments;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Vowels {
 	public static int countVowels(String str) {
 		int sum = 0;
@@ -8,30 +12,46 @@ public class Vowels {
 			char character = Character.toUpperCase(str.charAt(i));
 			
 			if (character == 'A' || character == 'E' || character == 'I' || character == 'O' || character == 'U') {
-				// Increment sum and print out vowel
+				// Increment sum 
 				sum++;
-				System.out.println(character);
 			}
 		}
 		
 		return sum;
 	}
 	
+	public static int countVowels(File file) throws FileNotFoundException {
+		// Open file to read input
+		Scanner input = new Scanner(file);
+		int sum = 0;
+		
+		// Go through file and count vowels
+		while (input.hasNext()) {
+			String line = input.nextLine();
+			
+			sum += countVowels(line);
+		}
+		
+		input.close();
+		return sum;
+	}
+	
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		// Test word
 		String word = "Test";
 		int result;
 		
+		File file = new File("C:\\\\Users\\\\dirtt\\\\eclipse-workspace\\\\Assignments\\\\src\\\\words.txt");
+		
 		// Intro message
 		System.out.println("Counting the vowels in the word: " + word);
-		System.out.println();
 		
 		result = countVowels(word);
-		
-		// Horizontal rule
-		System.out.println("------");
-		
 		System.out.println("Total: " + result);
+		System.out.println();
+		
+		System.out.println("Counting the vowels in the file: words.txt");
+		System.out.println("Total: " + countVowels(file));
 	}
 }
