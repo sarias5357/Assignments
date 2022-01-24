c3c951f4a9d940dee96f60784ef29280e2d76df7
2dirtyy <2dirttyy@gmail.com>
2022-01-24 00:19:17 -0500

    Completed assignment
    Count words

--- src/assignments/WordCount.java
+++ src/assignments/WordCount.java
@@ -7,16 +7,21 @@ import java.util.Scanner;
 public class WordCount {
 	public static int countWords(String x) {
 		String[] newX;
+		
+		// Split the string on anything that is not a word
 		newX = x.split("\\W+");
 		
 		return newX.length;
 	}
 	
 	public static int countWords(File file) throws FileNotFoundException {
+		// Open file to read input 
 		Scanner input = new Scanner(file);
+		
 		String[] str;
 		int sum = 0;
 		
+		// Go through file and count strings
 		while (input.hasNext()) {
 			str = input.nextLine().split("\\W+");
 			
@@ -28,17 +33,18 @@ public class WordCount {
 	}
 	
 	public static void main(String[] args) throws FileNotFoundException {
-		File file = new File("C:\\Users\\dirtt\\eclipse-workspace\\Assignments\\src\\words.txt");
+		File file = new File("C:\\Users\\dirtt\\eclipse-workspace\\Assignments\\src\\words.txt"); // Test file
 				
 		try {
 			System.out.println(countWords(file));
 		}
 		catch (FileNotFoundException e) {
+			// If file is invalid, terminate program
 			System.out.println("Invalid file");
 			System.exit(0);
 		}
 		finally {
-			System.out.println(countWords("hello, my name is steven!"));
+			System.out.println(countWords("hello, my name is steven!")); // Test string input
 		}
 	}
 }
